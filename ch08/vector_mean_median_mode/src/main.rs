fef89f111bac455b0b676e7d9139be26d93bbde0
carlosamolina <15368012+CarlosAMolina@users.noreply.github.com>
2021-05-17 23:01:14 +0200

    Partical commit. Chapter 08

---
+++ ch08/vector_mean_median_mode/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let vector = vec![5, 4, 1, 2, 3];
+    println!("Vector: {:?}", vector);
+    assert_eq!(3, mean(vector));
+}
+
+fn mean(vector: Vec<usize>) -> usize {
+    let mut sum = 0;
+    for number in vector.iter() {
+        sum += number;
+    }
+    let mean_result = sum / vector.len();
+    mean_result
+}
+
+
