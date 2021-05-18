f357ed22439af96ecd539080993d3a4509acad5a
carlosamolina <15368012+CarlosAMolina@users.noreply.github.com>
2021-05-18 22:34:53 +0200

    Ch08 add median

--- ch08/vector_mean_median_mode/src/main.rs
+++ ch08/vector_mean_median_mode/src/main.rs
@@ -1,13 +1,17 @@
 fn main() {
     let vector = vec![5, 4, 1, 2, 3];
     println!("Vector: {:?}", vector);
-    assert_eq!(3, mean(vector));
+    assert_eq!(3, mean(&vector));
+    assert_eq!(3, median(&vector));
+    assert_eq!(vec![5, 4, 1, 2, 3], vector);
 }
 
-fn mean(vector: Vec<usize>) -> usize {
-    let mut sum = 0;
-    for number in vector.iter() {
-        sum += number;
-    }
-    sum / vector.len()
+fn mean(vector: &[usize]) -> usize {
+    vector.iter().sum::<usize>() / vector.len()
+}
+
+fn median(vector: &Vec<usize>) -> usize {
+    let mut vector_aux = vector.clone();
+    vector_aux.sort();
+    vector_aux[vector.len() / 2]
 }
