bb84506bdc911d5ca03aa4dd85950a89259c96bf
carlosamolina <15368012+CarlosAMolina@users.noreply.github.com>
2021-06-01 22:26:28 +0200

    Partial commit. Ch80 hash map department. Update hash map vec

--- ch08/hash_map_department/src/main.rs
+++ ch08/hash_map_department/src/main.rs
@@ -2,28 +2,27 @@ use std::collections::HashMap;
 
 fn main() {
     let mut departments = HashMap::new();
-    departments.insert("Engineering", "Sally");
-    departments.insert("Sales", "Amir");
-    departments.insert("Delete", "Foo");
-    println!("{:?}", departments);
-    departments.remove("Delete");
 
-    let mut result_expected = HashMap::new();
-    result_expected.insert("Engineering", "Sally");
-    result_expected.insert("Sales", "Amir");
-    assert_eq!(result_expected, departments);
+    departments.insert("Engineering", vec![]);
+    departments.insert("Sales", vec![]);
 
-    let mut v: Vec<String> = vec!["a".to_string(),
-                                  "d".to_string(),
-                                  "e".to_string(),
-                                  "d".to_string(),
-                                  "b".to_string()];
-    v.push("a".to_string());
-    println!("{:?}", v);
-    let mut departments_b = HashMap::new();
-    departments_b.insert("vector".to_string(), v);
-    println!("{:?}", departments_b["vector"]);
-    //TODO departments_b["vector"].push("x".to_string());
+    departments.get_mut("Engineering").unwrap().push("Sally");
+    departments.get_mut("Sales").unwrap().push("Doe");
+    departments.get_mut("Sales").unwrap().push("Amir");
+    departments.get_mut("Sales").unwrap().sort();
 
+    let department = "Games";
+    let employee = "Carlos";
+    departments
+        .entry(department)
+        .and_modify(|v| v.push(employee))
+        .or_insert(vec![employee]);
+
+    add_employee(&departments, &department, &employee);
 }
 
+fn add_employee(departments: &HashMap<&str, Vec<&str>>, department: &str, employee: &str) {
+    println!("{:?}", departments);
+    println!("{:?}", department);
+    println!("{:?}", employee);
+}
