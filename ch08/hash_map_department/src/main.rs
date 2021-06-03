f8ac035900a4beb3a60da1ccfbcedf44701631f4
carlosamolina <15368012+CarlosAMolina@users.noreply.github.com>
2021-06-03 23:02:32 +0200

    Partial commit. Ch80 hash map department. Get user input

--- ch08/hash_map_department/src/main.rs
+++ ch08/hash_map_department/src/main.rs
@@ -1,33 +1,73 @@
 use std::collections::HashMap;
+use std::io;
 
 fn main() {
-    let mut departments = HashMap::new();
+    println!("Welcome to the departments menu!");
 
-    departments.insert("Engineering", vec![]);
-    departments.insert("Sales", vec![]);
+    let mut departments: HashMap<&str, Vec<String>> = HashMap::new();
 
-    departments.get_mut("Engineering").unwrap().push("Sally");
-    departments.get_mut("Sales").unwrap().push("Doe");
-    departments.get_mut("Sales").unwrap().push("Amir");
-    departments.get_mut("Sales").unwrap().sort();
+    loop {
+        let mut user_input = String::new();
+        let mut user_input_v: Vec<&str> = vec![];
 
-    let department = "Games";
-    let employee = "Carlos";
-    departments
-        .entry(department)
-        .and_modify(|v| v.push(employee))
-        .or_insert(vec![employee]);
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read line");
 
-    add_employee(&departments, &department, &employee);
+        let user_input: String = match user_input.trim().parse() {
+            Ok(user_input) => user_input,
+            Err(_) => {
+                println!("\nInvalid input :(");
+                continue;
+            }
+        };
 
-    let mut departments_name: Vec<_> = vec![];
-    for key in departments.keys() {
-        departments_name.push(key);
+        println!("User input : {:?}", user_input);
+        user_input_v = user_input.split(' ').collect();
+
+        if user_input.starts_with("Add") {
+            let department = user_input_v[3].to_string();
+            let employee = user_input_v[1].to_string();
+            departments.insert(user_input_v[3], vec![]);
+            //departments.get_mut("Engineering").unwrap().push("Sally".to_string());
+            /*
+            departments
+                    .entry(department)
+                    .and_modify(|v| v.push(employee.to_string()))
+                    .or_insert(vec![employee.to_string()]);
+                */
+            println!("{:?}", departments);
+        }
     }
-    departments_name.sort();
-    for name in departments_name {
-        println!("{:?}: {:?}", name, departments[name]);
-    }
+
+    /*
+
+        departments.insert("Engineering", vec![]);
+        departments.insert("Sales", vec![]);
+
+        departments.get_mut("Engineering").unwrap().push("Sally");
+        departments.get_mut("Sales").unwrap().push("Doe");
+        departments.get_mut("Sales").unwrap().push("Amir");
+        departments.get_mut("Sales").unwrap().sort();
+
+        let department = "Games";
+        let employee = "Carlos";
+        departments
+            .entry(department)
+            .and_modify(|v| v.push(employee.to_string()))
+            .or_insert(vec![employee.to_string()]);
+
+    //    add_employee(&departments, &department, &employee);
+
+        let mut departments_name: Vec<_> = vec![];
+        for key in departments.keys() {
+            departments_name.push(key);
+        }
+        departments_name.sort();
+        for name in departments_name {
+            println!("{:?}: {:?}", name, departments[name]);
+        }
+        */
 }
 
 fn add_employee(departments: &HashMap<&str, Vec<&str>>, department: &str, employee: &str) {
