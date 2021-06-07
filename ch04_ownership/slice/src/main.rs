b6cec2e6cd26b73206369a330b6d3e7b0402738c
carlosamolina <15368012+CarlosAMolina@users.noreply.github.com>
2021-06-08 01:23:33 +0200

    Ch04 slice: blank spaces index

--- ch04_ownership/slice/src/main.rs
+++ ch04_ownership/slice/src/main.rs
@@ -1,18 +1,18 @@
 fn main() {
-    let string = "Hello, world !".to_string();
-    //let word_1_last_index = first_word_last_index(&string[..]);
-    //let word_1 = first_word(&string[..]);
+    let string = "Add Carlos to develop team".to_string();
+    assert_eq!("Add", first_word(&string[..]));
+    assert_eq!(3, first_space_index(&string[..]));
+    assert_eq!((4, 10), second_word_indexes(&string[..]));
     let word_2_indexes = second_word_indexes(&string[..]);
-    //println!("First word: {}", word_1);
-    //println!("First word last index: {}", word_1_last_index);
-    println!("Second word indexes: {:?}", word_2_indexes);
-    println!(
-        "Second word: {:?}",
-        &string[word_2_indexes.0..word_2_indexes.1]
-    );
+    assert_eq!("Carlos", &string[word_2_indexes.0..word_2_indexes.1]);
+    let spaces_index = blank_spaces_index(&string);
+    assert_eq!(vec![3, 10, 13, 21], spaces_index);
+    assert_eq!("Carlos", &string[spaces_index[0] + 1..spaces_index[1]]);
+    assert_eq!("develop", &string[spaces_index[2] + 1..spaces_index[3]]);
+    assert_eq!("team", &string[spaces_index[3] + 1..string.len()]);
 }
 
-fn first_word_last_index(s: &str) -> usize {
+fn first_space_index(s: &str) -> usize {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
@@ -39,7 +39,7 @@ fn first_word(s: &str) -> &str {
 fn second_word_indexes(s: &str) -> (usize, usize) {
     let bytes = s.as_bytes();
     let mut first_space = 0;
-    let mut second_space = 0;
+    let second_space;
     for (i, &item) in bytes.iter().enumerate() {
         //println!("{}", i);
         //println!("{}", &item);
@@ -53,4 +53,15 @@ fn second_word_indexes(s: &str) -> (usize, usize) {
         }
     }
     (0, s.len())
+}
+
+fn blank_spaces_index(s: &str) -> Vec<usize> {
+    let bytes = s.as_bytes();
+    let mut indexes = vec![];
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            indexes.push(i);
+        }
+    }
+    indexes
 }
