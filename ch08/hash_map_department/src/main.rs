04cb69bc5fb34b030eb1bd872f3fa9f44d72cec5
carlosamolina <15368012+CarlosAMolina@users.noreply.github.com>
2021-06-09 23:16:06 +0200

    Partial commit. Ch08 hash map department. Add employee to department

--- ch08/hash_map_department/src/main.rs
+++ ch08/hash_map_department/src/main.rs
@@ -4,15 +4,16 @@ use std::io;
 fn main() {
     println!("Welcome to the departments menu!");
 
-    let mut departments: HashMap<&str, Vec<String>> = HashMap::new();
+    let mut departments: HashMap<String, Vec<String>> = HashMap::new();
 
     loop {
         let mut user_input = String::new();
-        let mut user_input_v: Vec<&str> = vec![];
+        //let mut user_input_v: Vec<&str> = vec![];
 
         io::stdin()
             .read_line(&mut user_input)
             .expect("Failed to read line");
+
 
         let user_input: String = match user_input.trim().parse() {
             Ok(user_input) => user_input,
@@ -23,19 +24,29 @@ fn main() {
         };
 
         println!("User input : {:?}", user_input);
-        user_input_v = user_input.split(' ').collect();
+
+
+        //user_input_v = user_input.split(' ').collect();
 
         if user_input.starts_with("Add") {
-            let department = user_input_v[3].to_string();
-            let employee = user_input_v[1].to_string();
-            departments.insert(user_input_v[3], vec![]);
+
+	    let spaces_index = blank_spaces_index(&user_input);
+            println!("spaces index: {:?}", spaces_index);
+
+            let employee = user_input[spaces_index[0] + 1..spaces_index[1]].to_string();
+            let department = &user_input[spaces_index[2] + 1..user_input.len()];
+
+            println!("employee: {:?}", employee);
+            //println!("department: {:?}", department);
+
+            //let department = user_input_v[3].to_string();
+            //let employee = user_input_v[1].to_string();
+            //departments.insert(employee, vec![]);
             //departments.get_mut("Engineering").unwrap().push("Sally".to_string());
-            /*
             departments
-                    .entry(department)
+                    .entry(department.to_string())
                     .and_modify(|v| v.push(employee.to_string()))
                     .or_insert(vec![employee.to_string()]);
-                */
             println!("{:?}", departments);
         }
     }
@@ -74,4 +85,15 @@ fn add_employee(departments: &HashMap<&str, Vec<&str>>, department: &str, employ
     println!("{:?}", departments);
     println!("{:?}", department);
     println!("{:?}", employee);
+}
+
+fn blank_spaces_index(s: &str) -> Vec<usize> {
+    let bytes = s.as_bytes();
+    let mut indexes = vec![];
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            indexes.push(i);
+        }
+    }
+    indexes
 }
