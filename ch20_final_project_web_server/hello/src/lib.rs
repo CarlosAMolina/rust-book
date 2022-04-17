7f05415e91fbf711f5ad0b80649994db0f30e69e
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-04-17 20:50:39 +0200

    Ch20: add sending requests to threads via channels

--- ch20_final_project_web_server/hello/src/lib.rs
+++ ch20_final_project_web_server/hello/src/lib.rs
@@ -1,8 +1,16 @@
+use std::sync::mpsc::Receiver;
 use std::thread;
+use std::sync::Arc;
+use std::sync::mpsc;
+use std::sync::Mutex;
+
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -15,12 +23,16 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id))
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -36,8 +48,8 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: mpsc::Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| { receiver; });
         Worker { id, thread }
     }
 }
