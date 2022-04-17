5397d19917417570c84884aef49dfa7d453347f6
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-04-17 22:15:46 +0200

    Ch20: add Signaling to the Threads to Stop Listening for Jobs

--- ch20_final_project_web_server/hello/src/bin/main.rs
+++ ch20_final_project_web_server/hello/src/bin/main.rs
@@ -14,13 +14,15 @@ fn main() {
 
     println!("Init on {}", url);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
